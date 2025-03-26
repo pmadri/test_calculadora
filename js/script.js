4170a0406f35fde9d5a1283d881f11abaf9f808a
pmadri <pedrophacv@gmail.com>
2025-03-26 20:34:30 -0300

    Método de soma, vetor Array e conversão de string em float

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
+class Calculator{
+
+    constructor(){
+        this.upperValue = document.querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number');
+        this.reset = 0;
+    }
+
+    //método de soma, parâmetros n1 e n2
+    sum(n1, n2){
+        return parseFloat(n1) + parseFloat(n2)
+    }
+
+    resolution(){
+        //nós jogamos para um array uma string
+        let upperValueArray = (this.upperValue.textContent). split(" ");
+        result = Calc.sum(upperValueArray[i - 1], this.upperValueArray[i + 1]);
+    }
+}
+
+//start nos btns
 let buttons = document.querySelectorAll('.btn');
-console.log(buttons);
+console.log('teste');
