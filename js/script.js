dd9fd4fe5127f1288c7ad716afe7f60755068b66
pmadri <pedrophacv@gmail.com>
2025-03-26 21:33:12 -0300

    Continuação da nova lógica do JS

--- js/script.js
+++ js/script.js
@@ -6,15 +6,26 @@ class Calculator{
         this.reset = 0;
     }
 
-    //método de soma, parâmetros n1 e n2
+    //método soma
     sum(n1, n2){
         return n1 + n2;
+    }
+
+    subtraction(n1, n2){
+        return n1 - n2;
     }
 }
 
 //criando o objeto
-const cal = new Calculator;
+const calc = new Calculator;
 
 //start nos btns
 let buttons = document.querySelectorAll('.btn');
-console.log('teste');
+
+//chamando o método de soma
+const resultado = calc.sum(1,2);
+console.log(resultado);
+
+//chamando o método de subtração
+let teste = calc.subtraction(5,4);
+console.log(teste);
