94cbb212811c3416075d372049a77afcd7015688
pmadri <pedrophacv@gmail.com>
2025-03-26 21:19:53 -0300

    Mudanças na lógica do JS

--- js/script.js
+++ js/script.js
@@ -8,15 +8,12 @@ class Calculator{
 
     //método de soma, parâmetros n1 e n2
     sum(n1, n2){
-        return parseFloat(n1) + parseFloat(n2)
-    }
-
-    resolution(){
-        //nós jogamos para um array uma string
-        let upperValueArray = (this.upperValue.textContent). split(" ");
-        result = Calc.sum(upperValueArray[i - 1], this.upperValueArray[i + 1]);
+        return n1 + n2;
     }
 }
+
+//criando o objeto
+const cal = new Calculator;
 
 //start nos btns
 let buttons = document.querySelectorAll('.btn');
